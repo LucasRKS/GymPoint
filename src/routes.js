8ab3f8d34c9b3816b5f7df5d0ae6af76c86f939e
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-10-25 11:32:14 -0300

    Criação de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
 // Apenas o roteamento do express
 import { Router } from 'express';
 
+// Controllers
+import User from './app/controllers/UserController';
+
 const routes = new Router();
+
+routes.post('/users', (req, res) => {
+  return User.store(req, res);
+});
 
 export default routes;
