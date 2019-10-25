b8aac91d573d390f41bf388d7cc15a7aa274c11c
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-10-24 21:17:38 -0300

    Adicionando Express, Nodemon, Sucrase e Debug

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+//Apenas o roteamento do express
+import { Router } from 'express';
+
+const routes = new Router();
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,7 @@
+/*
+  Configurçaões do servidor
+*/
+
+import app from './app';
+
+app.listen(3334);
