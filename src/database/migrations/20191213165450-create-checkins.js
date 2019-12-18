2ab25ef79867a56a97b21153bd32fdd93fff74ee
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-12-18 13:45:46 -0300

    Adicionando ID nas tables

--- src/database/migrations/20191213165450-create-checkins.js
+++ src/database/migrations/20191213165450-create-checkins.js
@@ -1,10 +1,15 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('checkins', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
       student_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        primaryKey: true,
         references: { model: 'students', key: 'id' },
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
