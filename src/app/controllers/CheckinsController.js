e8798e974549b0492c6376b0f154148974595114
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-12-18 15:58:34 -0300

    Checkins e validações de 7 dias

--- src/app/controllers/CheckinsController.js
+++ src/app/controllers/CheckinsController.js
@@ -1,3 +1,6 @@
+import { format, subDays } from 'date-fns';
+import { pt } from 'date-fns/locale';
+import { Op } from 'sequelize';
 import Checkin from '../models/Checkin';
 import Student from '../models/Student';
 
@@ -23,7 +26,35 @@ class CheckinsController {
   }
 
   async store(req, res) {
-    return res.json();
+    const student_id = req.params.id;
+
+    const subDate = subDays(new Date(), 7);
+    const today = new Date();
+
+    const currentCheckins = await Checkin.findAll({
+      where: {
+        student_id,
+        createdAt: { [Op.between]: [subDate, today] },
+      },
+    });
+
+    if (currentCheckins.length === 7) {
+      return res.status(400).json({
+        error: 'Student alredy used the gym for 7 consecutive days.',
+      });
+    }
+
+    const checkin = await Checkin.create({ student_id });
+
+    const student = await Student.findByPk(student_id);
+
+    const formatedCheckin = format(
+      checkin.createdAt,
+      "'Dia' dd 'de' MMMM 'de' yyyy 'às' HH':'mm",
+      { locale: pt }
+    );
+
+    return res.json({ checkin: formatedCheckin, student: student.name });
   }
 }
 
