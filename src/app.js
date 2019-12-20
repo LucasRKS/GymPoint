b7d98b5de248c4452fe42488625a9f6c8b22a410
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-12-20 12:04:57 -0300

    Configurando Sentry

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
-/*
-  Configurações do express
-*/
 import express from 'express';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
+import 'express-async-errors';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://118efff739684358ae36a9fbb9662af2@sentry.io/1863320',
+};
