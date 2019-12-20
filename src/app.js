736f4fc577464af7ff4f971c230c57790199e413
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2019-12-20 12:11:10 -0300

    Middleware de tratamento de erros e Youch

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 import routes from './routes';
 import sentryConfig from './config/sentry';
-
-import 'express-async-errors';
 
 import './database';
 
@@ -15,6 +15,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -25,6 +26,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, res).toJSON();
+
+      return res.status(500).json({ errors });
+    });
   }
 }
 
